22d381a5844a137a925d632cbd06069f295fd0c2
Xdecosee <finalfantasy_1998@hotmail.com>
2020-04-08 04:11:37 +0800

    insert buggy

--- App/db/dbsql.js
+++ App/db/dbsql.js
@@ -7,8 +7,9 @@ sql.query = {
     login: 'SELECT * FROM Users WHERE username=$1',
 
     //Restaurant Staff
-    restInfo: 'SELECT * FROM Restaurants R INNER JOIN RestaurantStaff RS on R.restaurantID =  RS.restaurantID WHERE RS.uid = $1',
-    menuInfo: 'SELECT * FROM Food F INNER JOIN Restaurants R on F.restaurantID = R.restaurantID INNER JOIN RestaurantStaff RS on R.restaurantID =  RS.restaurantID WHERE RS.uid = $1',
+    restInfo: 'SELECT * FROM Restaurants R INNER JOIN RestaurantStaff RS on R.restaurantID =  RS.restaurantID WHERE RS.uid = $1 LIMIT 1',
+    menuInfo: 'SELECT * FROM Food F INNER JOIN Restaurants R on F.restaurantID = R.restaurantID WHERE R.restaurantID = $1',
+    insertFood: 'INSERT INTO Food(foodName, price, category, RestaurantID) Values($1, $2, \'Indian Cuisine\', $3)',
 
     //Guide_files
     all_users: 'SELECT uid, name, username FROM Users',
